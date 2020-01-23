8a60d9f30d0631b9b6549063839ead4ef994880d
Tymovskyi <ded.lviv@gmail.com>
2020-01-23 08:40:15 +0200

    added base graphl

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,17 @@
-import * as cors from "cors";
-import * as express from "express";
-import * as helmet from "helmet";
-import * as hpp from "hpp";
-import * as logger from "morgan";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import logger from "morgan";
 import errorMiddleware from "./middlewares/error.middleware";
+import { ApolloServer } from "apollo-server-express";
+import query from "qs-middleware";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolves";
 
+const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
+const path = "/graphql";
 
 app.use(hpp());
 app.use(helmet());
@@ -13,7 +19,9 @@ app.use(logger("combined"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(errorMiddleware);
+app.use(query());
+
+server.applyMiddleware({ app, path });
 
 export default app;
